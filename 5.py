307583d9211f15125e2b6f7328e317cb294c4133
Eduard Wojnar <168775964+Aknud3@users.noreply.github.com>
2024-11-23 14:24:54 +0100

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,18 +1,21 @@
 class Solution(object):
     """Vytvořím si objekt, který bude mít 2 funkce. Objekt se jmenuje Solution(řešení), a řešení má 2 možnosti, buď complicated nebo short"""
 
-    def generate_n_rectangles_complicated(self, number_of_rectangles):
+    # do funkce vložíme počet čtverců a velikost vektoru o jaký to chceme posouvat
+    def generate_n_rectangles_complicated(self, number_of_rectangles, vector_length):
         """Tohle je funkce, která je náročná. Nejprve udělá prázdné pole plné '  ', potom do středu dá # poté pomocí vektorů tyto # duplikuje
-        i do rohů dalšího čtverce a pokračuje dokuď není v poslední vrstvě. 
-        Potom se spojí rohy čárou a vektorem se znovu posunou, ale teď do menšího čtverce."""
-        
-        array = [] # hlavní pole ve kterém se vše děje
-        list_of_sizes = [] # pole se všemi velikosti čtverců (používá se pro for loopy)
+        i do rohů dalšího čtverce a pokračuje dokuď není v poslední vrstvě.
+        Potom se spojí rohy čárou a vektorem se znovu posunou, ale teď do menšího čtverce.
+        """
 
-        
+        array = []  # hlavní pole ve kterém se vše děje
+        list_of_sizes = []  # pole se všemi velikosti čtverců (používá se pro for loopy)
+
         # Tento loop přidá do list_of_sizes všechny velikosti
         for i in range(number_of_rectangles):
-            list_of_sizes.append(i+1) # Python počítá od 0, tak tam potřebuju + 1, protože moje čtverce začínají od velikosti 1
+            list_of_sizes.append(
+                i + 1
+            )  # Python počítá od 0, tak tam potřebuju + 1, protože moje čtverce začínají od velikosti 1
 
         # Velikost finálního pole
         # 1 = 1, 2 = 5, 3 = 9, 4 = 13, ...
@@ -24,44 +27,55 @@ class Solution(object):
             array.append(row)
 
         # Vytvoření # v centru
-        center = -2 # center jestli jede jednou tak je na 0,0 proto začíná proměnná na -2
+        center = (
+            -2
+        )  # center jestli jede jednou tak je na 0,0 proto začíná proměnná na -2
         for i in range(list_of_sizes[-1]):
             center += 2
-        array[center][center] = "# " # určíš souřadnicemi kde je # (y, x)
+        array[center][center] = "# "  # určíš souřadnicemi kde je # (y, x)
 
-        vectors = [[2, 2], [2, -2], [-2, 2], [-2, -2]] # určím si vektory které budu používat pro posun
-        list_of_centers = [[center, center]] # list souřadnic se kterým chci pracovat
-        visited = set(tuple(corner) for corner in list_of_centers) # toto je list souřadnic které jsem navštívil, set je speciální druh listu, kde nejsou duplikáty a je neměný, využívá se při hledání
+        # v defaultu to je [[2,2], [2,-2], [-2,2], [-2,-2]]
+        vectors = [
+            [vector_length, vector_length],
+            [vector_length, -vector_length],
+            [-vector_length, vector_length],
+            [-vector_length, -vector_length],
+        ]  # určím si vektory které budu používat pro posun
+        list_of_centers = [[center, center]]  # list souřadnic se kterým chci pracovat
+        visited = set(
+            tuple(corner) for corner in list_of_centers
+        )  # toto je list souřadnic které jsem navštívil, set je speciální druh listu, kde nejsou duplikáty a je neměný, využívá se při hledání
 
-        
-        
         for _ in list_of_sizes:
-            new_list_of_centers = [] # list nových středů
-            
+            new_list_of_centers = []  # list nových středů
+
             # Tato smička mi veme střed a udělá z něho petern, co je třeba na kostce, když ukazuje 5 (zduplikování symbolů do rohů)
             for cord in list_of_centers:
-                x = cord[0] # nastavení bodu x
-                y = cord[1] # nastavení bodu y
-                
-                # Toto jede pro každý vektor ve vektorech 
+                x = cord[0]  # nastavení bodu x
+                y = cord[1]  # nastavení bodu y
+
+                # Toto jede pro každý vektor ve vektorech
                 for vector in vectors:
-                    dx, dy = vector # Pro vektor [2, 2] je dx 2 a dy 2, pro vektor [2, -2] to bude dx 2 a dy -2
-                    nx, ny = x + dx, y + dy # Tohle udělá nx a ny což je součet souřadnice cord[0] a cord[1] a to se akorát sečte s vektorem, který jsme si vytvořili o řádek nahoře
-                    
+                    dx, dy = (
+                        vector  # Pro vektor [2, 2] je dx 2 a dy 2, pro vektor [2, -2] to bude dx 2 a dy -2
+                    )
+                    nx, ny = (
+                        x + dx,
+                        y + dy,
+                    )  # Tohle udělá nx a ny což je součet souřadnice cord[0] a cord[1] a to se akorát sečte s vektorem, který jsme si vytvořili o řádek nahoře
+
                     # Podmínka, co jenom hlídá jestli nikde nepřesahujeme a už jsme nenavštívili ten bod
-                    if (
-                        0 <= nx < size
-                        and 0 <= ny < size
-                        and (nx, ny) not in visited
-                    ):
+                    if 0 <= nx < size and 0 <= ny < size and (nx, ny) not in visited:
                         # Jestli vše splňujeme na souřadnice nx a ny se hodí #
                         array[nx][ny] = "# "
-                        new_list_of_centers.append([nx, ny]) # Uložíme si souřadnice do nových corners, protože tato funkce poběží i pro tyto vytvořené body
-                        visited.add((nx, ny)) # Uložíme si bod i do navštívených bodů
-                    list_of_centers = new_list_of_centers # Změníme si list_of_centers na nový a smyčka jede tolikrát kolik máme čtverců
+                        new_list_of_centers.append(
+                            [nx, ny]
+                        )  # Uložíme si souřadnice do nových corners, protože tato funkce poběží i pro tyto vytvořené body
+                        visited.add((nx, ny))  # Uložíme si bod i do navštívených bodů
+                    list_of_centers = new_list_of_centers  # Změníme si list_of_centers na nový a smyčka jede tolikrát kolik máme čtverců
             # Tato funkce nám udělala šachovnici s '# ' nebo '  ', která má ale mezery 2 (je to extrémně překoplikované řešení)
-        
-        # vytvoříme si list souřadnic, kde jsou všechny rohy pole array      
+
+        # vytvoříme si list souřadnic, kde jsou všechny rohy pole array
         list_of_corners = [
             [0, 0],
             [0, size - 1],
@@ -69,7 +83,6 @@ class Solution(object):
             [size, size - 1],
         ]
 
-        
         for _ in list_of_sizes:
             # znovu si vytvoříme pole nových rohů
             new_list_of_corners = []
@@ -78,44 +91,57 @@ class Solution(object):
             top_left, top_right, bottom_left, bottom_right = list_of_corners
 
             # pro každé x v poli horní levý a horní pravý x + 1 dáme #
-            # Tohle jen že zadáme 2 souřadnice a podle osy x nám to nakreslí rovnou čáru '# ' 
+            # Tohle jen že zadáme 2 souřadnice a podle osy x nám to nakreslí rovnou čáru '# '
             for x in range(top_left[1], top_right[1] + 1):
                 array[top_left[0]][x] = "# "
-                
-            # Tohle je to stejné akorát pro spodní rohy    
+
+            # Tohle je to stejné akorát pro spodní rohy
             for x in range(bottom_left[1], bottom_right[1] + 1):
                 array[bottom_left[0]][x] = "# "
-                
+
             # Toto je to stejné ale místo osy x to je pro osu y
             # Toto kreslí levou stěnu
             for y in range(top_left[0], bottom_left[0]):
                 array[y][top_left[1]] = "# "
-                
+
             # Toto kreslí pravou stěnu
             for y in range(top_right[0], bottom_right[0]):
                 array[y][top_right[1]] = "# "
 
-            # Teď si pouze všechny rohy posunu o 2 do středu, zase pomocí vektoru (je to napsané jinak, ale je to stále vektor)
-            new_list_of_corners.append([top_left[0] + 2, top_left[1] + 2])
-            new_list_of_corners.append([top_right[0] + 2, top_right[1] - 2])
-            new_list_of_corners.append([bottom_left[0] - 2, bottom_left[1] + 2])
-            new_list_of_corners.append([bottom_right[0] - 2, bottom_right[1] - 2])
+            # Teď si pouze všechny rohy posunu o 2 do středu, zase pomocí vektoru
+            new_list_of_corners.append(
+                [top_left[0] + vector_length, top_left[1] + vector_length]
+            )
+            new_list_of_corners.append(
+                [top_right[0] + vector_length, top_right[1] - vector_length]
+            )
+            new_list_of_corners.append(
+                [bottom_left[0] - vector_length, bottom_left[1] + vector_length]
+            )
+            new_list_of_corners.append(
+                [bottom_right[0] - vector_length, bottom_right[1] - vector_length]
+            )
 
             # Změníme list rohů
             list_of_corners = new_list_of_corners
         # Tato smyčka jede zase podle toho kolik máme čtverců a z šachovnice udělá finální výtvor
-        
-        # Celé pole vytiskneme
-        for row in array: # pro linku v poli
-            for char in row: # pro symbol v lince
-                print(char, end="") # tisk symbolu buď '# ' nebo '  '
-            print("") # odřádkování
 
+        # Celé pole vytiskneme
+        for row in array:  # pro linku v poli
+            for char in row:  # pro symbol v lince
+                print(char, end="")  # tisk symbolu buď '# ' nebo '  '
+            print("")  # odřádkování
+
+    # do funkce vložíme jenom celkový počet čtverců
     def generate_n_rectangles_short(self, number_of_rectangles):
         # Toto není moje řešení, ale napadlo to člena soutěže TdA
-        size = 1 + ((number_of_rectangles - 1) * 4) # nastavení velikosti celkového pole
+        size = 1 + (
+            (number_of_rectangles - 1) * 4
+        )  # nastavení velikosti celkového pole
 
-        for y in range(size): # pro y v size (v pythnu jak jsme už psal nahoře se první bere osa Y, protože když máme 2 rozměrné pole, tak máme 2 závorky [0][0] s dvěmi indexy a první
+        for y in range(
+            size
+        ):  # pro y v size (v pythnu jak jsme už psal nahoře se první bere osa Y, protože když máme 2 rozměrné pole, tak máme 2 závorky [0][0] s dvěmi indexy a první
             # první ukazuje v jakém subpoli se pohybujeme a druhé ukazuje v jakém prvku toho sub pole jsme, takže jestli máme tohle:
             # [
             # ['a', 'b', 'c'],
@@ -124,7 +150,7 @@ class Solution(object):
             # ]
             # tak to je jedno velké pole kde jsou 3 malá pole, první nám ukazuje v jakém poli se pohybujeme pole[0] = pismenka, pole[1] = čísla, pole[2] = souřadnice a teprve druhá nám
             # ukazuje kde v tom druhém poli jsme pole[0][0] = a, pole[2][2] = y
-            
+
             # Takže ta smička nahoře jede pro velká pole a tohle pro každý ten jednotlivý prvek
             for x in range(size):
                 # Toto je if vložené do funcke a říká to: vytiskni # pokud minimální hodnota z x,y size-x-1, size-y-1 je dělitelná 2 beze zbytku, jinak vytiskni '  '
@@ -140,9 +166,11 @@ class Solution(object):
 
 # Vytvoření 2 objektů solution a každá zavolá jinou funkci
 
-Solution().generate_n_rectangles_complicated(6)
-print()
-Solution().generate_n_rectangles_short(6)
 
-# Snad tento komentář někomu pomohl pochopit python lépe. Moje řešení tohoto problému nebylo nejlepší, co se týče jednoduchosti, ale k výsledku jsem se dostal.
+Solution().generate_n_rectangles_complicated(3, 2)
+print()
+Solution().generate_n_rectangles_short(3)
+
+# Snad tento komentář někomu pomohl pochopit python lépe. Moje řešení tohoto problému nebylo nejlepší, co se týče jednoduchosti, ale k výsledku jsem se dostal, navíc to může generovat i
+# jiné věci když se změní vektor
 # Druhé řešení považuji za správné, jen mě to prostě nenapadlo. Creator: Víťa aka sprtokiller
